969d370c58d3d56e95da1532287d82e60bb4f5e8
Bill Welliver <bill@welliver.org>
2006-02-21 06:08:32 +0000

    work on combo picker
    --HG--
    extra : convert_revision : svn%3A2b5a5965-8a46-0410-aa19-a76fe43533fc/trunk%402013

--- static/javascripts/menulayer.js
+++ static/javascripts/menulayer.js
@@ -1,6 +1,7 @@
 dojo.require("dojo.html");
 dojo.require("dojo.fx.*");
 dojo.require("dojo.widget.html.DatePicker");
+dojo.require("dojo.widget.ComboPicker")
 dojo.require("dojo.io.IframeIO");
 
 var menuLayers = {
