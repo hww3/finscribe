3bc25b93ca1f3c427df5c80f52855a41fde076ac
Bill Welliver <bill@welliver.org>
2006-05-10 22:00:04 +0000

    for auto-ajaxification
    --HG--
    extra : convert_revision : svn%3A2b5a5965-8a46-0410-aa19-a76fe43533fc/trunk%402187

---
+++ static/javascripts/bootstrap.js
@@ -0,0 +1,46 @@
+var fs_global = this;
+
+function setupAjaxLinks()
+{
+  if(!(typeof fs_global["dojo"] == "undefined"))
+  {
+    doSetupAjaxLinks();
+  }
+}
+
+function doSetupAjaxLinks()
+{
+   convertToPopup("BackLinks"); 
+   convertToPopup("PostComment"); 
+   convertToPopup("TrackBacks"); 
+   convertToPopup("PingBacks"); 
+
+   convertToAjax("Login", openLogin);
+   convertToAjax("PostBlog", openPostBlog);
+   convertToAjax("Actions", openActions);
+
+   if(!dj_undef("doThemeSetup"))
+     doThemeSetup();
+} 
+
+function convertToAjax(id, func)
+{
+  var a = document.getElementById(id);
+
+  if(!a) return;
+  a.onclick = function(event){ if(!event) event = window.event; func(a, event); return false; };
+
+  a.href="#";
+}
+
+function convertToPopup(id)
+{
+  var a = document.getElementById(id);
+
+  if(!a) return;
+
+  var z = a.href;
+  a.onclick = function(event){ openPopup(z + "?ajax=1", "80%"); return false; };
+
+  a.href="#";
+}
